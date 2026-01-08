d6ceb9ffaa119d7b0c0af80967ba9d06f0a5f876
Anish Velagapudi <velagapudi26a@ncssm.edu>
2026-01-08 13:27:19 -0500

    Comment Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       anish                                                     */
-/*    Created:      10/9/2025, 5:12:18 PM                                     */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
+//main.cpp
 
 #include "vex.h"
 
@@ -17,11 +10,7 @@ brain Brain;
 // A global instance of competition
 competition Competition;
 
-motor frontLeft = motor(PORT10, false);
-motor frontRight = motor(PORT20, true);
-motor backLeft = motor(PORT15, false);;
-motor backRight = motor(PORT2, true);
-controller Controller1;
+
 //inertial inertialSensor = inertial(PORT10);
 
 
@@ -29,49 +18,24 @@ controller Controller1;
 
 int deadzone = 5;
 
-// define your global instances of motors and other devices here
+//global instances of motors and other devices here
+motor frontLeft = motor(PORT10, false);
+motor frontRight = motor(PORT20, true);
+motor backLeft = motor(PORT15, false);;
+motor backRight = motor(PORT2, true);
+controller Controller1;
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
+
+
 
 void pre_auton(void) {
 
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+  // All activities that occur before the competition starts (ex:  clearing encoders, setting servo positions ...)
 }
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  // ..........................................................................
   // Insert autonomous user code here.
-  // ..........................................................................
 }
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
   // User control code here, inside the loop
@@ -128,28 +92,23 @@ int displayTemperatures() {
     Brain.Screen.newLine();
     Brain.Screen.print("Back Right: %.1f f", backRight.temperature(fahrenheit));
 
-    wait(500, msec); // update twice per second
+    wait(1000, msec); // 1 time per second
   }
   return 0;
 }
 
 
 
-//
-// Main will set up the competition functions and callbacks.
-//
+//Competition Function Setup and Callback
 int main() {
-  // Set up callbacks for autonomous and driver control periods.
+  
 
-  thread displayThread = thread(displayTemperatures);
 
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
 
-  // Run the pre-autonomous function.
   pre_auton();
 
-  // Prevent main from exiting with an infinite loop.
   while (true) {
     wait(100, msec);
   }
